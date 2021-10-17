e7373b9b7909bcbbc1fddc7bf52b3474e7014f6d
irfanaqeel <64663725+irfanaqeel@users.noreply.github.com>
2021-10-17 12:41:12 +0000

    Add files via upload

---
+++ Arifa'Hb/public/script.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter UserName");
+
+  	if(a== "Arifa")
+  	{
+  	    window.location.href="Birthday.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
---
+++ Hbirth/public/js/name.js
@@ -0,0 +1,10 @@
+var a=prompt("Enter UserName");
+   if(a== "muneeza")
+   {
+       window.location.href="name.html";
+   }
+   else
+   {
+     alert("invalid details");
+     window.location.href = "index.html"
+   }
